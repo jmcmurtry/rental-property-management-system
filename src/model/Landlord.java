0eac454e11b90550c00a84c5f32f3c895bf78252
arule11 <47372331+arule11@users.noreply.github.com>
2021-12-01 20:36:24 -0700

    Update Landlord.java

--- src/model/Landlord.java
+++ src/model/Landlord.java
@@ -1,5 +1,41 @@
 package model;
 
-public class Landlord {
-    
+/* 
+ * Subject.java
+ * ENSF 480 - Project
+ * 
+ * John McMurtry 30087058
+ * Athena McNeil-Roberts 30042085
+ * Arindam Mishra-30092921
+ * Harrison Mondragon 30088805
+ */
+
+import java.util.ArrayList;
+
+public class Landlord extends UserProfile{
+    ArrayList<Property> properties;
+
+    public Landlord(String email, String pswd){
+        super(email, pswd);
+    }
+
+    public void addProperty(Property p){
+        properties.add(p);
+    }
+
+    public void addProperty(String type, int bed, int bath, boolean furn, String quad, String email){
+    }
+
+    public void removeProperty(int id){
+        for(int i = 0; i < properties.size(); i++){
+            if(properties.get(i).getID() == id){
+                properties.remove(i);
+            }
+        }
+    }
+
+    public void payFee(int propId){
+        // create payment object and set to that properties paymentInfo
+    }
+
 }
