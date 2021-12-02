9f3acc0cccc7be8d52b124e2cb0204f3b3bba76b
arule11 <47372331+arule11@users.noreply.github.com>
2021-12-01 20:36:44 -0700

    Update Date.java

--- src/model/Date.java
+++ src/model/Date.java
@@ -1,5 +1,42 @@
 package model;
 
+/* 
+ * Subject.java
+ * ENSF 480 - Project
+ * 
+ * John McMurtry 30087058
+ * Athena McNeil-Roberts 30042085
+ * Arindam Mishra-30092921
+ * Harrison Mondragon 30088805
+ */
+
 public class Date {
+    private int year;
+    private int month;
+    private int day;
     
+    public Date(int y, int m, int d){
+    }
+
+    public void setYear(int y){
+    }
+
+    public void setMonth(int m){
+    }
+
+    public void setDay(int d){
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public int getMonth(){
+        return this.month;
+    }
+
+    public int getDay(){
+        return this.day;
+    }
+
 }
