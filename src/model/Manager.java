acb911a6e3f0fd34f290d8db0687c5c949f24107
arule11 <47372331+arule11@users.noreply.github.com>
2021-12-01 20:55:36 -0700

    Update Manager.java

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -1,5 +1,32 @@
 package model;
 
-public class Manager {
-    
+/* 
+ * Manager.java
+ * ENSF 480 - Project
+ * 
+ * John McMurtry 30087058
+ * Athena McNeil-Roberts 30042085
+ * Arindam Mishra-30092921
+ * Harrison Mondragon 30088805
+ */
+
+
+public class Manager extends UserProfile{
+
+    public Manager(String email, String pswd){
+        super(email, pswd);
+    }
+
+    public void changeListingState(String newState, int propertyID){
+    }
+
+    public void changeFees(int newFee, Property p){
+    }
+
+    public void changePeriod(Date start, Date end, Property p){
+    }
+
+    public SummaryReport generateReport(Date start, Date end){
+    }
+
 }
