2eae31a7d7f9ab4bf75270e0348f9e07146f7e66
arule11 <47372331+arule11@users.noreply.github.com>
2021-12-01 20:35:55 -0700

    Update RegisteredRenter.java

--- src/model/RegisteredRenter.java
+++ src/model/RegisteredRenter.java
@@ -1,5 +1,47 @@
 package model;
 
-public class RegisteredRenter {
-    
+/* 
+ * Subject.java
+ * ENSF 480 - Project
+ * 
+ * John McMurtry 30087058
+ * Athena McNeil-Roberts 30042085
+ * Arindam Mishra-30092921
+ * Harrison Mondragon 30088805
+ */
+
+import java.util.ArrayList;
+
+public class RegisteredRenter extends UserProfile implements Subject {
+    private ArrayList<Observer> observers;
+    private ArrayList<Property> filteredProperties;
+    private ArrayList<Property> propertiesRented;
+
+    public RegisteredRenter(String email, String pswd){
+        super(email, pswd);
+    }
+
+    public void register(Observer o){
+        observers.add(o);
+        o.update(filteredProperties);
+    }
+
+    public void remove(Observer o){
+        observers.remove(o);
+    }
+
+    public void notifyObserver(){
+        for(int i = 0; i < observers.size(); i++){
+            Observer o = observers.get(i);
+            o.update(filteredProperties);
+        }
+    }
+
+    public void unsubscribe(){
+    }
+
+    public void addProperty(Property p){
+        propertiesRented.add(p);
+    }
+
 }
