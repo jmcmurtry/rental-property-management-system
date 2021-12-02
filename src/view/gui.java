10e878ca183decc62c87a6d0b5658c0473c67bf7
unknown <mondragon.harrison@gmail.com>
2021-12-01 22:41:36 -0700

    added comments

--- src/view/gui.java
+++ src/view/gui.java
@@ -1,3 +1,14 @@
+/* 
+ * gui.java
+ * ENSF 480 - Project
+ * 
+ * John McMurtry 30087058
+ * Athena McNeil-Roberts 30042085
+ * Arindam Mishra-30092921
+ * Harrison Mondragon 30088805
+ */
+
+
 public class gui {
     
 }
