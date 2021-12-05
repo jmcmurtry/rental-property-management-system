0d77cb238e033e4b66bf7d87b6f598952cd2a951
arule11 <47372331+arule11@users.noreply.github.com>
2021-12-04 21:23:50 -0700

    Update SummaryReport.java

---
+++ src/model/SummaryReport.java
@@ -0,0 +1,49 @@
+
+package model;
+
+class SummaryReport {
+    private int totalListed;
+    private int totalRented;
+    private int totalActive;
+    private ArrayList<Property> housesRented;
+
+    public SummaryReport(int listed, int rented, int active, ArrayList<Property> houses){
+        this.totalListed = listed;
+        this.totalRented = rented;
+        this.totalActive = active;
+        this.housesRented = houses;
+    }
+
+    public void setTotalListed(int listed){
+        this.totalListed = listed;
+    }
+
+    public void setTotalRented(int rented){
+        this.totalRented = rented;
+    }
+
+    public void setTotalActive(int active){
+        this.totalActive = active;
+    }
+
+    public void setHousesRented(ArrayList<Property> houses){
+        this.housesRented = houses;
+    }
+
+    public int getTotalListed(){
+        return this.totalListed;
+    }
+
+    public int getTotalRented(){
+        return this.totalRented;
+    }
+
+    public int getTotalActive(){
+        return this.totalActive;
+    }
+
+    public ArrayList<Property> getHousesRented(){
+        return this.housesRented;
+    }
+    
+}
