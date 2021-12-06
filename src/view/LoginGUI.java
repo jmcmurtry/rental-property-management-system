b2e9918453b2313210b5f90797ad135bbe05d3e1
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2021-12-06 02:54:47 -0700

    updated GUI classes

--- src/view/LoginGUI.java
+++ src/view/LoginGUI.java
@@ -21,31 +21,32 @@ public class LoginGUI {
     private JButton loginButton;
     private JButton guestButton;
     private JLabel successfulLogin;
+    private JButton registerButton;
 
     public LoginGUI(){
 
         JFrame frame = new JFrame("Welcome");
-        JPanel panel = new JPanel();
+        JPanel loginPanel = new JPanel();
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);   
-        panel.setLayout(null);
+        frame.add(loginPanel);   
+        loginPanel.setLayout(null);
 
         userLabel = new JLabel("Username:");
         userLabel.setBounds(10, 20, 80, 25);
-        panel.add(userLabel);
+        loginPanel.add(userLabel);
 
         usernameText = new JTextField(20); 
         usernameText.setBounds(100, 20, 165, 25);
-        panel.add(usernameText);
+        loginPanel.add(usernameText);
         
         passwordLabel = new JLabel("Password:");
         passwordLabel.setBounds(10, 50, 80, 25);
-        panel.add(passwordLabel);
+        loginPanel.add(passwordLabel);
 
         passwordText = new JPasswordField(20); 
         passwordText.setBounds(100, 50, 165, 25);
-        panel.add(passwordText);
+        loginPanel.add(passwordText);
 
         loginButton = new JButton("Login");
         loginButton.setBounds(20, 80, 80, 25);
@@ -54,19 +55,34 @@ public class LoginGUI {
                 String username = usernameText.getText();
                 String password = passwordText.getText();
                 System.out.println(username + ", " + password);
+                
             }
         });
-        panel.add(loginButton);
+        loginPanel.add(loginButton);
+
+        registerButton = new JButton("Register");
+        registerButton.setBounds(20, 110, 80, 25);
+        registerButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                Driver.registerButtonPressed();
+                frame.dispose();
+            }
+        });
+        loginPanel.add(registerButton);        
 
         successfulLogin = new JLabel("");
         successfulLogin.setBounds(10, 110, 300, 25);
-        panel.add(successfulLogin);
+        loginPanel.add(successfulLogin);
 
         guestButton = new JButton("Continue as guest");
         guestButton.setBounds(120, 80, 160, 25);
-        panel.add(guestButton);
+        guestButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+            }
+        });   
+        loginPanel.add(guestButton);             
 
-        frame.setContentPane(panel); 
+        frame.setContentPane(loginPanel); 
         frame.setVisible(true);
     }       
 }
--- src/view/RenterGUI.java
+++ src/view/RenterGUI.java
@@ -12,6 +12,65 @@ import java.awt.event.*;
  * Harrison Mondragon 30088805
  */
 
-public class LoginGUI {
+public class RenterGUI {
+    private CardLayout layout;
 
+    private JFrame frame = new JFrame("Renter Menu");
+    private JPanel panel = new JPanel(); 
+    
+    private JPanel menuPanel;
+    private JPanel searchPanel;       
+    private JPanel subscriptionsPanel;
+
+    private JLabel welcomeLabel;
+    private JButton searchButton;
+    private JButton subscriptionsButton;
+
+
+    public RenterGUI(){
+        frame.setSize(750, 750);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.add(panel);
+        frame.setVisible(true); 
+        
+        // initializing the menu panel and adding buttons
+        menuPanel = new JPanel();
+        menuPanel.setLayout(null);
+
+        searchPanel = new JPanel();
+        searchPanel.setLayout(null);  
+        
+        subscriptionsPanel = new JPanel();
+        subscriptionsPanel.setLayout(null);         
+
+        welcomeLabel = new JLabel("Welcome!");
+        welcomeLabel.setBounds(10, 20, 200, 25);
+        menuPanel.add(welcomeLabel);     
+        
+        searchButton = new JButton("Search for Properties");
+        searchButton.setBounds(10, 50, 200, 25); 
+        searchButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                layout.show(panel, "2");
+            }
+        });      
+        menuPanel.add(searchButton);   
+        
+        subscriptionsButton = new JButton("View Subscriptions");
+        subscriptionsButton.setBounds(10, 80, 200, 25); 
+        subscriptionsButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                layout.show(panel, "3");
+            }
+        });      
+        menuPanel.add(subscriptionsButton);  
+
+        layout = new CardLayout();
+        panel.setLayout(layout);
+        panel.add(menuPanel, "1");
+        panel.add(searchPanel, "2");
+        panel.add(subscriptionsPanel, "3");
+        layout.show(panel, "1");  
+    }
 }
