acbea24d325d5090a4c4776a27815e608a1be4d3
Arindam Mishra <77895599+arindammishra1999@users.noreply.github.com>
2021-12-10 15:59:02 -0700

    Update Email.java

--- src/model/Email.java
+++ src/model/Email.java
@@ -24,25 +24,51 @@ public class Email {
         this.propertyID = property;
         this.message = message;
     }
+    
+	/**
+	 * Getter method for emailID
+	 * @return returns an int corresponding to the emailID
+	 */
 
     public int getEmailId(){
         return this.emailId;
     }
+    
+	/**
+	 * Getter method for landlordID
+	 * @return returns an int corresponding to the landlordID
+	 */
 
     public int getLandlordID(){
         return this.landlordID;
     }
 
+	/**
+	 * Getter method for the renter email
+	 * @return returns a string corresponding to the renter's email address
+	 */
+    
     public String getRenterEmail(){
         return this.renterEmail;
     }
 
+	/**
+	 * Getter method for propertyID
+	 * @return returns an int corresponding to the propertyID
+	 */
+    
     public int getPropertyID(){
         return this.propertyID;
     }
 
+	/**
+	 * Getter method for message
+	 * @return returns a string corresponding to the message sent by the renter 
+	 */
+    
     public String getMessage(){
         return this.message;
     }
     
 }
+
