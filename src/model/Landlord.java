6929846ecbe7ac5ef29fec2334534f090ba6e601
Arindam Mishra <77895599+arindammishra1999@users.noreply.github.com>
2021-12-10 16:00:00 -0700

    Documentation

--- src/model/Landlord.java
+++ src/model/Landlord.java
@@ -15,17 +15,32 @@ import java.util.ArrayList;
 public class Landlord extends UserProfile{
     ArrayList<Property> properties;
 
+    
+	/**
+	 * Constructor of Landlord that calls the UserProfile constructor 
+	 * to inherit all the User's methods like getID, getEmail, setEmail, set and get password etc
+	 */
     public Landlord(String name, String email, String pswd){
         super(name, email, pswd);
     }
 
+    
+	/**
+	 * Method to add the property passed in to an ArrayList of properties
+	 * @param p
+	 */
     public void addProperty(Property p){
         properties.add(p);
     }
 
+    
     public void addProperty(String type, int bed, int bath, boolean furn, String quad, String email){
     }
 
+	/**
+	 * Method to remove the property corresponding to the id passed in from the ArrayList of properties
+	 * @param id
+	 */
     public void removeProperty(int id){
         for(int i = 0; i < properties.size(); i++){
             if(properties.get(i).getID() == id){
@@ -39,3 +54,4 @@ public class Landlord extends UserProfile{
     }
 
 }
+
